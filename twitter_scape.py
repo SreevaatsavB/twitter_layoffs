2008f884aed549be58f8abf0d00eba5c6e222576
Sreevaatsav Bavana <80395869+SreevaatsavB@users.noreply.github.com>
2023-02-24 16:29:27 +0530

    Update twitter_scape.py

--- twitter_scape.py
+++ twitter_scape.py
@@ -8,7 +8,6 @@ import os
 def get_tweets(curr_date, prev_date):
 
     query = '"layoffs" (#techlayoffs OR #layoffs OR #layoff) lang:en until:{} since:{}-filter:replies'
-    print(query_f)
     query_f = query.format(curr_date, prev_date)
     tweets = []
     limit = 1000000
@@ -26,21 +25,75 @@ def get_tweets(curr_date, prev_date):
     print(df1.shape)
     return df1
 
-if __name__ == 'main':
+# if __name__ == 'main':
 
-    curr_date = datetime.datetime.today().__str__()
-    prev_date = datetime.datetime.today() - datetime.timedelta(days=1)
-    prev_date = prev_date.__str__()
+curr_date = datetime.datetime.today().__str__()
+prev_date = datetime.datetime.today() - datetime.timedelta(days=1)
+prev_date = prev_date.__str__()
 
-    df_tweets = get_tweets(curr_date, prev_date)
+df_tweets = get_tweets(curr_date, prev_date)
 
-#     curr_dir = os.getcwd()
+print(df_tweets.shape)
 
-#     filename = "{}/tweets_data/twitter_{}_to_{}.json".format(curr_dir,prev_date, curr_date)
-    filename = "twitter_{}_to_{}.json".format(prev_date, curr_date)
+filename = "twitter_{}_to_{}.json".format(prev_date, curr_date)
+
+# print(os.listdir())
+
+df_tweets.to_json(filename)
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# import pandas as pd
+# import snscrape.modules.twitter as sntwitter
+# import datetime
+# import os
+
+
+# def get_tweets(curr_date, prev_date):
+
+#     query = '"layoffs" (#techlayoffs OR #layoffs OR #layoff) lang:en until:{} since:{}-filter:replies'
+#     print(query_f)
+#     query_f = query.format(curr_date, prev_date)
+#     tweets = []
+#     limit = 1000000
+
+#     i = 1
+#     for tweet in sntwitter.TwitterSearchScraper(query_f).get_items():
+#         if len(tweets) == limit:
+#             break
+#         else:
+#             tweets.append([tweet.date, tweet.username, tweet.content, tweet.likeCount, tweet.replyCount, tweet.retweetCount, tweet.retweetedTweet, tweet.user.location])
+            
+#         i += 1
+
+#     df1 = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'likeCount', 'replyCount', 'retweetCount', 'retweetedTweet', 'location'])
+#     print(df1.shape)
+#     return df1
+
+# if __name__ == 'main':
+
+#     curr_date = datetime.datetime.today().__str__()
+#     prev_date = datetime.datetime.today() - datetime.timedelta(days=1)
+#     prev_date = prev_date.__str__()
+
+#     df_tweets = get_tweets(curr_date, prev_date)
+
+# #     curr_dir = os.getcwd()
+
+# #     filename = "{}/tweets_data/twitter_{}_to_{}.json".format(curr_dir,prev_date, curr_date)
+#     filename = "twitter_{}_to_{}.json".format(prev_date, curr_date)
     
-    print(os.listdir())
+#     print(os.listdir())
 
-    df_tweets.to_json(filename)
+#     df_tweets.to_json(filename)
 
 
