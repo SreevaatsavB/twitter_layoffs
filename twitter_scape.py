cddf2ca7037ba40b4f2b1ff069912b0823178828
Sreevaatsav Bavana <80395869+SreevaatsavB@users.noreply.github.com>
2023-02-25 10:46:00 +0530

    Update twitter_scape.py

--- twitter_scape.py
+++ twitter_scape.py
@@ -4,7 +4,6 @@ import snscrape.modules.twitter as sntwitter
 import datetime
 import os
 
-
 def get_tweets(curr_date, prev_date):
 
     query = '"layoffs" (#techlayoffs OR #layoffs OR #layoff) lang:en until:{} since:{}-filter:replies'
@@ -27,8 +26,6 @@ def get_tweets(curr_date, prev_date):
     print(df1.shape)
     return df1
 
-# if __name__ == 'main':
-
 curr_date = datetime.datetime.today().date().__str__()
 prev_date = datetime.datetime.today() - datetime.timedelta(days=1)
 prev_date = prev_date.date().__str__()
@@ -38,66 +35,3 @@ df_tweets = get_tweets(curr_date, prev_date)
 filename = "twitter_{}_to_{}.json".format(prev_date, curr_date)
 
 df_tweets.to_json(filename)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import os
-# import pandas as pd
-# import snscrape.modules.twitter as sntwitter
-# import datetime
-# import os
-
-
-# def get_tweets(curr_date, prev_date):
-
-#     query = '"layoffs" (#techlayoffs OR #layoffs OR #layoff) lang:en until:{} since:{}-filter:replies'
-#     print(query_f)
-#     query_f = query.format(curr_date, prev_date)
-#     tweets = []
-#     limit = 1000000
-
-#     i = 1
-#     for tweet in sntwitter.TwitterSearchScraper(query_f).get_items():
-#         if len(tweets) == limit:
-#             break
-#         else:
-#             tweets.append([tweet.date, tweet.username, tweet.content, tweet.likeCount, tweet.replyCount, tweet.retweetCount, tweet.retweetedTweet, tweet.user.location])
-            
-#         i += 1
-
-#     df1 = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'likeCount', 'replyCount', 'retweetCount', 'retweetedTweet', 'location'])
-#     print(df1.shape)
-#     return df1
-
-# if __name__ == 'main':
-
-#     curr_date = datetime.datetime.today().__str__()
-#     prev_date = datetime.datetime.today() - datetime.timedelta(days=1)
-#     prev_date = prev_date.__str__()
-
-#     df_tweets = get_tweets(curr_date, prev_date)
-
-# #     curr_dir = os.getcwd()
-
-# #     filename = "{}/tweets_data/twitter_{}_to_{}.json".format(curr_dir,prev_date, curr_date)
-#     filename = "twitter_{}_to_{}.json".format(prev_date, curr_date)
-    
-#     print(os.listdir())
-
-#     df_tweets.to_json(filename)
-
-
